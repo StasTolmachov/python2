87cd37d1fcc57dbaeb9dff1f6def7ca9b9122943
StasTolmachov <Stas@Tolmachov.dev>
2023-11-15 08:48:45 +0100

    while

--- itproger/01.py
+++ itproger/01.py
@@ -1,4 +1,64 @@
-print(23)
-print(23)
-print(23)
-print(23)
+# print(1, 2, 3, 4, 5, sep='-', end='')
+# print('second \nprint')
+# print(round(5 / 3))
+
+# user_data = int(input('enter number: '))
+#
+# if user_data > 5:
+#     print('more than 5')
+# else:
+#     print('less than 5')
+#
+# data = 'five'
+# number = 5 if data == 'five' else 0
+# print(number)
+# word = 'Hello world!'
+# count = 0
+# for i in word:
+#     if i == 'l':
+#         count += 1
+#
+# print(count)
+
+# for i in range(1, 11):
+#     if i > 5:
+#         break
+#     print(i)
+
+# nums = [0, 1, 2, 3]
+# print(type(nums))
+# print(nums[-1])
+#
+# nums.append(11)
+# nums.insert(2, 22)
+# nums.extend([5, 6, 7])
+# nums2 = [8, 9, 10]
+# nums.extend(nums2)
+# print(f'data in list: {nums}')
+#
+# nums.sort()
+# print(f'sort data in list: {nums}')
+# nums.pop(2)
+# nums.remove(11)
+# print(f'sort data in list: {nums}')
+# # nums.clear()
+# print(f'sort data in list: {nums}')
+#
+# print(f'count sort data in list: {nums.count(1)}')
+# print(f'len sort data in list: {len(nums)}')
+
+# nums = [0, 1, 2, 'str', False]
+# for el in nums:
+#     print()
+
+
+# n = int(input('length: '))
+# userList = []
+# i = 0
+# while i < n:
+#     string = 'enter data in ' + str(i + 1) + ': '
+#     userList.append(input(string))
+#     i += 1
+#
+# print(userList)
+
